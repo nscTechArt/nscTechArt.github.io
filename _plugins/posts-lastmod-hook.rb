66972d7c64f7c4bfe47cbc4e1c61780e5be610b3
nscTechArt <2051303427@qq.com>
2026-03-11 17:24:37 +0800

    protected-posts：初步实现加密功能

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -11,4 +11,31 @@ Jekyll::Hooks.register :posts, :post_init do |post|
     post.data['last_modified_at'] = lastmod_date
   end
 
+  # Mark posts under locked folders, so templates can filter them out globally.
+  post.data['protected_by_folder_lock'] = false
+
+  lock_cfg = post.site.config['post_folder_lock']
+  if lock_cfg && lock_cfg['enabled']
+    post_path_down = post.path.to_s.downcase
+    folders = lock_cfg['folders'] || []
+
+    folders.each do |folder_item|
+      folder_path = nil
+
+      if folder_item.is_a?(Hash)
+        folder_path = folder_item['path']
+      else
+        folder_path = folder_item
+      end
+
+      next if folder_path.nil? || folder_path.to_s.empty?
+
+      if post_path_down.include?(folder_path.to_s.downcase)
+        post.data['protected_by_folder_lock'] = true
+        post.data['feed'] = false if post.data['feed'].nil?
+        break
+      end
+    end
+  end
+
 end
