7eeef389a626982da6e72b94973124cf25bcdd10
nscTechArt <2051303427@qq.com>
2026-03-11 18:30:51 +0800

    protected-posts：加密逻辑修复

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -13,10 +13,12 @@ Jekyll::Hooks.register :posts, :post_init do |post|
 
   # Mark posts under locked folders, so templates can filter them out globally.
   post.data['protected_by_folder_lock'] = false
+  post.data['folder_lock_scope'] = nil
 
   lock_cfg = post.site.config['post_folder_lock']
   if lock_cfg && lock_cfg['enabled']
-    post_path_down = post.path.to_s.downcase
+    # Normalize path separators so matching works consistently on Windows/macOS/Linux.
+    post_path_down = post.path.to_s.tr('\\', '/').downcase
     folders = lock_cfg['folders'] || []
 
     folders.each do |folder_item|
@@ -30,8 +32,17 @@ Jekyll::Hooks.register :posts, :post_init do |post|
 
       next if folder_path.nil? || folder_path.to_s.empty?
 
-      if post_path_down.include?(folder_path.to_s.downcase)
+      normalized_folder_path = folder_path.to_s.tr('\\', '/').downcase
+      next if normalized_folder_path.empty?
+
+      if post_path_down.include?(normalized_folder_path)
         post.data['protected_by_folder_lock'] = true
+        scope_source = if folder_item.is_a?(Hash)
+                         folder_item['category'] || folder_item['path']
+                       else
+                         folder_item
+                       end
+        post.data['folder_lock_scope'] = Jekyll::Utils.slugify(scope_source.to_s)
         post.data['feed'] = false if post.data['feed'].nil?
         break
       end
